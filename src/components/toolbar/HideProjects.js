e6e5c786d0bd2e52a61aa6c2d653f105be5dd8f9
sduckett <simonjduckett@hotmail.co.uk>
2022-04-26 11:24:55 +0100

    change icons to bars

--- src/components/toolbar/HideProjects.js
+++ src/components/toolbar/HideProjects.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const HideProjects = () => {
     const slideAway = () => {
         document.getElementById('projectsCol').classList.toggle('moveleft')
     }
-    return (<FontAwesomeIcon onClick={slideAway} icon={faArrowLeft} /> );
+    return (<FontAwesomeIcon onClick={slideAway} icon={faBars} /> );
 }
  
 export default HideProjects;
