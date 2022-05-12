df429eb416910a0d4e42b2cc89b3e9494868a158
sduckett <simonjduckett@hotmail.co.uk>
2022-05-12 11:32:41 +0100

    install toggle slider for theme

--- src/components/toolbar/ToggleTheme.js
+++ src/components/toolbar/ToggleTheme.js
@@ -1,10 +1,19 @@
+import styles from '../../styles/toggleSlider.module.scss'
+
 const ToggleTheme = () => {
     const toggleTheme = () => {
         document.body.classList.toggle('light')
     }
 
     return ( 
-        <button onClick={toggleTheme}>Dark</button>
+        // <button onClick={toggleTheme}>Dark</button>
+        <div style={{height: '0px'}}>
+            {/* <button onClick={toggleTheme}>Theme</button> */}
+            <input onClick={toggleTheme} type="checkbox" id="toggle" class={styles.toggle_checkbox} />
+            <label for="toggle" class={styles.toggle_label}>
+                <span class="toggle--label-background"></span>
+            </label>
+        </div>
      );
 }
  
